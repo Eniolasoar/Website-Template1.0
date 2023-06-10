1f76dada8ce9de3902e7a054c1b4b8d989cef241
Eniolasoar <eniolanishe@gmail.com>
2023-06-10 12:09:07 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let slideIndex=0;
+//call the funtion
+slideshow();
+
+function slideshow(){
+    //don't add . for classname
+    let slides=document.getElementsByClassName("slide");
+    //attach the loop parameters properly
+    for(let i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+    }
+    //increment the slideIndex
+    slideIndex++;
+    if(slideIndex>=slides.length){slideIndex=0};
+    slides[slideIndex].style.display="block";
+    //the time interval should be inside the function
+    setTimeout(slideshow,2000);
+   
+    
+    
+}
+
+var typed=new Typed(".auto-type",{
+    strings:["Welcome To SOARACADEMY "],
+    typeSpeed:150,
+    loop:true
+});
+
+
